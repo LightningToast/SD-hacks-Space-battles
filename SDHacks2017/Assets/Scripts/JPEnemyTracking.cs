d56cdd69106203607b979abdc80f01845559e945
Justin Park <justindypark@yahoo.com>
2017-10-21 16:20:24 -0700

    Added 3D models

---
+++ SDHacks2017/Assets/Scripts/JPEnemyTracking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JPEnemyTracking : MonoBehaviour {
+	GameObject[] enemies;
+	public float maxRange;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int count = 0; count < enemies.Length; count ++) {
+			if (Vector3.Distance(this.transform.position, enemies[count].transform.position) > maxRange) {
+				
+			}
+		}
+	}
+}
